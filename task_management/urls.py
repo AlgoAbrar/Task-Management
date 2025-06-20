eb671ce1ced6983cd6a2e01e087f2c1394cc1bbd
Saiyedul Abrar <saiyedul.abrar1430@gmail.com>
2025-06-20 15:37:07 +0600

    update

--- task_management/urls.py
+++ task_management/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from tasks.views import home
-from tasks.views import contact
+from tasks.views import contact, show_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home, name="home"),
     path("contact/", contact, name="contact"),
-    path("task/", include())
+    path("task/",show_task)
 ]
